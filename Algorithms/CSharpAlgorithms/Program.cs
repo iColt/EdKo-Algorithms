89ec2cffc2a5e6bc511dd49ab836d050735d32fa
eduard.kobzik <eduard.kobzik@gmail.com>
2020-01-03 16:24:30 +0300

    Added structure

---
+++ Algorithms/CSharpAlgorithms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Algorithms/MathAlgorithms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MathAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
