d99ebf64ac8ffd465a1ef33a61d40f825d61be58
Eduard <eduard.kobzik@gmail.com>
2020-01-05 22:03:45 +0300

    add bsc template

---
+++ Algorithms/DataStructures/Trees/BinarySearchTree.cs
@@ -0,0 +1,35 @@
+﻿using EdKo_Algorithms.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EdKo_Algorithms.Trees
+{
+    public class BinarySearchTree<T> where T : IComparable<T>
+    {
+        private BinarySearchTreeNode<T> _root { get; set; }
+        private Func<T,T,int> _compare { get; set; }
+        public BinarySearchTree()
+        {
+
+        }
+
+        public BinarySearchTree(BinarySearchTreeNode<T> root, IComparer<T> comparer)
+        {
+            _root = root;
+        }
+
+        public void Add(T item)
+        {
+
+        } 
+
+        private BinarySearchTreeNode<T> FindNode(T item, BinarySearchTreeNode<T> current, EqualType type)
+        {
+            var resultOfCompare = item.CompareTo(current.Data);
+
+
+
+        }
+    }
+}
---
+++ Algorithms/DataStructures/Trees/BinarySearchTreeNode.cs
@@ -0,0 +1,15 @@
+﻿namespace EdKo_Algorithms.Trees
+{
+    public class BinarySearchTreeNode<T>
+    {
+        public T Data { get; set; }
+        private BinarySearchTreeNode<T> _left { get; set; }
+        private BinarySearchTreeNode<T> _right { get; set; }
+
+        public BinarySearchTreeNode(BinarySearchTreeNode<T> left, BinarySearchTreeNode<T> right)
+        {
+            _left = left;
+            _right = right;
+        }
+    }
+}
